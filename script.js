5bcf3f888eac7cb14614048e8804e245d5318cee
VitezslavK <vitecekkubis1@gmail.com>
2025-02-17 14:43:43 +0100

    prochazka

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-
-egree
+function fce(pocetHvezdicek){
+    let vystup = "";
+    for (let i=1; i<=pocetHvezdicek; i++) {
+        vystup +="*";
+    }
+    return vystup;
+}
+function ctverec(){
+    let vystup = "";
+    for (let i=1; i<=10; i++){
+        vystup += fce(i) + "\n";
+    }
+    return vystup;
+}
+console.log(ctverec());

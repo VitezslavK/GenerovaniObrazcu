683daac53c6905a89996c5fe566f983f70203981
VitezslavK <vitecekkubis1@gmail.com>
2025-02-17 15:34:48 +0100

    uprava ve skole

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-function fce(pocetHvezdicek){
+function radekHvezdicek(pocetHvezdicek){
     let vystup = "";
     for (let i=1; i<=pocetHvezdicek; i++) {
         vystup +="*";
     }
     return vystup;
 }
-function ctverec(){
+function trojuhelnik(){
     let vystup = "";
     for (let i=1; i<=10; i++){
-        vystup += fce(i) + "\n";
+        vystup += radekHvezdicek(i) + "<br>";
     }
     return vystup;
 }
-console.log(ctverec());
+const btnTrojuhelnik = document.querySelector("#btnTrojuhelnik");
+const vystup = document.querySelector("main");
+btnTrojuhelnik.onclick = function(){
+    vystup.innerHTML = trojuhelnik();
+}
